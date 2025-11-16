4a5772355b83cb5371e4966b6773d825d1df7134
mcdonaldmark <markamcdonald7@gmail.com>
2025-11-15 21:15:04 -0700

    commit

--- src/js/checkout.js
+++ src/js/checkout.js
@@ -1,25 +1,35 @@
-import { loadHeaderFooter } from "./utils.mjs";
+import { loadHeaderFooter, qs } from "./utils.mjs";
 import CheckoutProcess from "./CheckoutProcess.mjs";
+import ShoppingCart from "./ShoppingCart.mjs";
 
-loadHeaderFooter();
+// wait for header/footer to load
+loadHeaderFooter().then(() => {
+  // initialize cart counter
+  const cart = new ShoppingCart("so-cart", ".product-list");
 
-const myCheckout = new CheckoutProcess("so-cart", ".checkout-summary");
-myCheckout.init();
+  function updateCartCounter(total, itemsCount) {
+    const countEl = qs(".cart-count");
+    if (!countEl) return;
+    countEl.textContent = itemsCount || 0;
+  }
 
-document
-  .querySelector("#zip")
-  .addEventListener("blur", myCheckout.calculateOrdertotal.bind(myCheckout));
-// listening for click on the button
-document.querySelector("#checkoutSubmit").addEventListener("click", (e) => {
-  e.preventDefault();
+  cart.onChange(updateCartCounter);
+  cart.init();
 
-  myCheckout.checkout();
+  // initialize checkout process
+  const myCheckout = new CheckoutProcess("so-cart", ".checkout-summary");
+  myCheckout.init();
+
+  // zip blur listener
+  document
+    .querySelector("#zip")
+    .addEventListener("blur", myCheckout.calculateOrdertotal.bind(myCheckout));
+
+  // checkout button listener
+  document
+    .querySelector("#checkoutSubmit")
+    .addEventListener("click", (e) => {
+      e.preventDefault();
+      myCheckout.checkout();
+    });
 });
-
-// this is how it would look if we listen for the submit on the form
-// document.forms['checkout']
-// .addEventListener('submit', (e) => {
-//   e.preventDefault();
-//   // e.target would contain our form in this case
-//    myCheckout.checkout();
-// });
