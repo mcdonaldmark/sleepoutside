7fe76106e4829ef8e1d2f0c24b3af7bf990a9dab
mcdonaldmark <markamcdonald7@gmail.com>
2025-11-20 22:21:45 -0700

    commit

--- src/js/ProductData.mjs
+++ src/js/ProductData.mjs
@@ -1,3 +1,5 @@
+const baseURL = import.meta.env.VITE_SERVER_URL;
+
 function convertToJson(res) {
   if (res.ok) {
     return res.json();
@@ -7,20 +9,19 @@ function convertToJson(res) {
 }
 
 export default class ProductData {
-  constructor() {}
-
-  async getData(category) {
-    // Load JSON files directly from the public folder on Netlify.
-    const response = await fetch(`/${category}.json`);
-    return await convertToJson(response);
+  constructor() {
+    // this.category = category;
+    // this.path = `../public/json/${this.category}.json`;
   }
-
-  async findProductById(id) {
-    // Product pages also need to pull from a single JSON source.
-    // Load all product data and find the matching ID.
-    const response = await fetch(`/all-products.json`);
-    const products = await convertToJson(response);
-
-    return products.find((item) => item.Id === id);
+async getData(category) {
+  const response = await fetch(`/json/${category}.json`);
+  const data = await convertToJson(response);
+  return data;
   }
+  
+async findProductById(id) {
+  const response = await fetch(`/json/all-products.json`);
+  const products = await convertToJson(response);
+  return products.find((item) => item.Id === id);
+}
 }
