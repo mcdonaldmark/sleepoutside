2642024ba7f63ba18d60d84138a085203eab11ce
mcdonaldmark <markamcdonald7@gmail.com>
2025-11-22 05:40:26 -0700

    commit

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
 import { resolve } from "path";
 import { defineConfig } from "vite";
 
-export default defineConfig(({ mode }) => ({
+export default defineConfig({
   root: "src/",
-  base: mode === "production" ? "/sleepoutside/" : "/",
+  base: "/sleepoutside/", // GitHub Pages base
   build: {
     outDir: "../dist",
     rollupOptions: {
@@ -11,9 +11,10 @@ export default defineConfig(({ mode }) => ({
         main: resolve(__dirname, "src/index.html"),
         cart: resolve(__dirname, "src/cart/index.html"),
         checkout: resolve(__dirname, "src/checkout/index.html"),
-        product: resolve(__dirname, "src/product_pages/index.html"),
-        product_listing: resolve(__dirname, "src/product_listing/index.html")
-      }
-    }
-  }
-}));
+        checkout_success: resolve(__dirname, "src/checkout/success.html"),
+        product_listing: resolve(__dirname, "src/product_listing/index.html"),
+        product_page: resolve(__dirname, "src/product_pages/index.html"),
+      },
+    },
+  },
+});
